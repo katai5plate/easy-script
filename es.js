e5ad4c2450be361f8914ea49373698165a357723
katai5plate <katai5plate.sub@gmail.com>
2018-01-10 11:42:06 +0900

    add help

--- es.js
+++ es.js
@@ -1,15 +1,44 @@
 (
     {
-        // 配列の要素数だけループする処理を少しスマートに。
-        /* e$.afor([array],(value,index,array,max,per)=>{}) */
+        help: (c) => {
+            switch (c) {
+                case "afor":
+                    console.log("配列の要素数だけループする処理を少しスマートに。")
+                    console.log('es.afor([4,5,6],(v,i,a,m,p)=>{console.log([v,i,a,m,p])});')
+                    break;
+                case "ofor":
+                    console.log("aforと同じようにオブジェクトをループ")
+                    console.log('es.ofor({one:1,two:2,three:3},(v,i,a,m,p)=>{console.log([v,i,a,m,p])});')
+                    break;
+                case "rep":
+                    console.log("任意の回数だけループ")
+                    console.log('es.rep(10,(i,m,p)=>{console.log([i,m,p])});')
+                    break;
+                case "gkey":
+                    console.log("連想配列を含む配列から特定のキーを抽出し配列化。3番目は詰めるかどうか")
+                    console.log('console.log(es.gkey([{a:7,b:1},{b:2},{a:9,c:1}],"a",true));')
+                    break;
+                case "sum":
+                    console.log("配列の合計値を取得")
+                    console.log('console.log(es.sum([1,2,3,4,5,6,7,8,9]));')
+                    break;
+                case "avr":
+                    console.log("配列の平均値を取得")
+                    console.log('console.log(es.avr([1,2,3,4,5,6,7,8,9]));')
+                    break;
+                default:
+                    console.log('es.help("COMMAND")');
+                    break;
+            }
+
+        },
+
         afor: (a, f) => {
             for (let i = 0; i < a.length; i++) {
                 f(a[i], i, a, a.length, (i + 1) / a.length);
             }
         },
 
-        // aforと同じようにオブジェクトをループ
-        /* e$.ofor({object},(value,index,array,max,per)=>{}) */
         ofor: (o, f) => {
             let i = 0;
             Object.keys(o).forEach(k => {
@@ -18,14 +47,10 @@
             });
         },
 
-        // 任意の回数だけループ
-        /* e$.rep(count,(index,max,per)=>{}) */
         rep: (c, f) => {
             for (let i = 0; i < c; i++) f(i, c, (i + 1) / c);
         },
 
-        //連想配列を含む配列から特定のキーを抽出し配列化
-        /* e$.gkey([{},{}...],key,Pack?) */
         gkey: (a, k, p) => {
             let r = [];
             for (let i in a) {
@@ -38,7 +63,6 @@
             return r;
         },
 
-        //配列の合計値を取得
         sum: (a) => {
             let r = 0;
             for (let i = 0; i < a.length; i++) {
@@ -47,7 +71,6 @@
             return r;
         },
 
-        //配列の平均値を取得
         avr: (a) => {
             let r = 0;
             for (let i = 0; i < a.length; i++) {
